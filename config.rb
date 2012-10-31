5abdb96818948fdc15c533cc9812478a576933d4
Mike Chen <chagel@gmail.com>
2012-10-31 18:28:38 +0800

    update config for static cache

--- config.rb
+++ config.rb
@@ -56,22 +56,28 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :cache_buster
+  activate :cache_buster
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
 
-  # Or use a different image path
+  # Or use a different image path - only work when not relative_assets
   # set :http_path, "/Content/images/"
+
+  # specify assets host
+  # activate :asset_host
+  # set :asset_host do |asset|
+  #   "http://assets%d.example.com" % (asset.hash % 4)
+  # end
 end
