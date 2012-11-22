02e9011b9323bb5252b88d803d7e1f25b0e6e847
Mike Chen <chagel@gmail.com>
2012-11-22 10:47:48 +0800

    add cdn for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Terrier::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://www.joyplus.tv"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( active_admin.js active_admin.css active_admin/print.css kindeditor_custom.js index.css countdown.css jquery.countdown.min.js jquery.min.js slides.min.jquery.js)
